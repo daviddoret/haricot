de75be6d50ffe9bdf589145c66511996c6df9360
DM3D <david.doret@me.com>
2018-07-28 15:41:55 +0200

    More binary functions

---
+++ R/BiFun_0010.R
@@ -0,0 +1,38 @@
+library(R6);
+
+#' @rdname BiFun_xxxx
+BiFun_0010 <- R6Class(
+  "BiFun_0010",
+  inherit = NandTree,
+  public = list(
+    initialize = function() {
+      super$initialize(input_dimension = 2, output_dimension = 1);
+
+      # Inverse the first input bit.
+      self$set_nand_subnode(
+        subnode_id = "n1",
+        param1_id = "i1",
+        param2_id = "i1");
+
+      # Apply NAND to the second bit and the inverse of the first input bit.
+      self$set_nand_subnode(
+        subnode_id = "n2",
+        param1_id = "i2",
+        param2_id = "n1");
+
+      # Inverse the result.
+      self$set_nand_subnode(
+        subnode_id = "n3",
+        param1_id = "n2",
+        param2_id = "n2");
+
+      self$set_output_subnode(
+        subnode_id = "o1",
+        param1_id = "n3");
+
+    },
+    do_randomize_outputs = function() {
+      stop("Not supported");
+    }
+  )
+)
