6e334d25614cc64854b93a0454f7ea536c56a19d
DM3D <david.doret@me.com>
2018-08-04 02:00:58 +0200

    Basic composition tests

--- tests/testthat/test-AlgoComposite.R
+++ tests/testthat/test-AlgoComposite.R
@@ -46,6 +46,51 @@ test_that('CompositeAlgo: faking NAND', {
 
 });
 
+
+test_that('CompositeAlgo: faking NOT', {
+
+  #browser();
+
+  input_dimension <- 1;
+  output_dimension <- 1;
+
+  a <- AlgoComposite$new(
+    input_dimension = input_dimension,
+    output_dimension = output_dimension);
+
+  expect_equal(a$get_input_dimension(), input_dimension);
+  expect_equal(a$get_output_dimension(), output_dimension);
+
+  nand1 <- AlgoNAND$new();
+
+  a$set_inner_node(node = nand1);
+
+  a$set_inner_edge(
+    source_node_id = a$get_node_id(),
+    source_bit_id = "i1",
+    target_node_id = nand1$get_node_id(),
+    target_bit_id = "i1");
+
+  a$set_inner_edge(
+    source_node_id = a$get_node_id(),
+    source_bit_id = "i1",
+    target_node_id = nand1$get_node_id(),
+    target_bit_id = "i2");
+
+  a$set_inner_edge(
+    source_node_id = nand1$get_node_id(),
+    source_bit_id = "o1",
+    target_node_id = a$get_node_id(),
+    target_bit_id = "o1");
+
+  a$do_plot();
+
+  expect_equal(a$do_execute("0"), "1");
+  expect_equal(a$do_execute("1"), "0");
+
+});
+
+
 test_that('CompositeAlgo: basic composition', {
 
   #browser();
