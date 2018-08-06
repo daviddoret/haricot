edfb47ee717afb68ebe888998e8e8cdb49bfc701
DM3D <david.doret@me.com>
2018-08-06 05:38:40 +0200

    Add html doc with pkgdown::build_site()
    A great package!

--- R/_build.R
+++ R/_build.R
@@ -5,13 +5,34 @@
 #someotherscript()
 #build()
 
+# DESCRIPTION
+#desc::desc_add_author(
+#  given = "David",
+#  family = "DORET",
+#  email = "david.doret@me.com",
+#  role = "aut",
+#  comment = NULL, file = ".", normalize = FALSE)
+#desc::desc_add_author(
+#  given = "David",
+#  family = "DORET",
+#  email = "david.doret@me.com",
+#  role = "cph",
+#  comment = NULL, file = ".", normalize = FALSE)
+#desc::desc_add_author(
+#  given = "David",
+#  family = "DORET",
+#  email = "david.doret@me.com",
+#  role = "cre",
+#  comment = NULL, file = ".", normalize = FALSE)
+
+
 # HTML DOCUMENTATION
 #
 # Install release version from CRAN
 #install.packages("pkgdown")
 # Install development version from GitHub
 # devtools::install_github("r-lib/pkgdown")
-#library(pkgdown)
-#pkgdown::build_site()
+# library(pkgdown)
+# pkgdown::build_site()
 
 
